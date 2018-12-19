c44e67a36ddc8b76bc554a462bfa3583ec3796cf
Earl Manning <earl.manning@arm.com>
2018-12-19 13:31:08 -0600

    Took out debug; added readme
    Signed-off-by: Earl Manning <earl.manning@arm.com>

---
+++ Readme.md
@@ -0,0 +1,10 @@
+# ButtonAdd
+
+This is a Chrome extension to add a button to the bottom of a `os.mbed.com/questions/*` page.  The button will create an issue in the IOTDEV project in Jira.
+
+#### Current Status
+1. It does not actually do anything with the data.  It just fills out a form and then nothing.  It needs the JIRA connection.
+
+
+#### Todo
+1. Segment out the work for os.mbed.com so that we can use a similar technique for zendesk.
--- background.js
+++ background.js
@@ -11,10 +11,10 @@ chrome.runtime.onMessage.addListener(function(request) {
                 tabId: tab.id,
                 focused: true
             }, function(win) {
-              console.log("title: " + request.title);
-              console.log("descr: " + request.descr);
-              console.log("author: " + request.author);
-              console.log("time: " + request.reporttime);
+              // console.log("title: " + request.title);
+              // console.log("descr: " + request.descr);
+              // console.log("author: " + request.author);
+              // console.log("time: " + request.reporttime);
 
               chrome.tabs.sendMessage(tab.id, {action: "populate_jira_fields",
                 title: request.title,
